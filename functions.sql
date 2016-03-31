d34832401fb0b6c956473985218645c74ac58209
manoj <manoj@rudrainnovatives.com>
2016-03-31 13:49:03 +0530

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -3,3 +3,6 @@ str VARCHAR( 255 ) ,
 delim VARCHAR( 12 ) ,
 pos INT
 ) RETURNS VARCHAR( 255 ) RETURN REPLACE( SUBSTRING( SUBSTRING_INDEX( str, delim, pos ) , LENGTH( SUBSTRING_INDEX( str, delim, pos -1 ) ) +1 ) , delim,  '' )
+
+
+SELECT SPLIT_STRING('apple, pear, melon', ',', 1)
